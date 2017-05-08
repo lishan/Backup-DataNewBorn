e0d6e4f1866e8329a695de15dfc416287421d08f
yuls3 <yulishan3@gmail.com>
2017-05-08 21:59:13 +0800

    ng table dynamic example

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,10 +1,11 @@
-'use strict'
+'use strict';
 
 /**
  * Controller of the dashboard
  */
 angular.module('dataNewBorn')
-  .controller('DashboardCtrl', ['$rootScope', '$scope', '$uibModal', 'NgTableParams', '$http', function ($rootScope, $scope, $uibModal, NgTableParams, $http) {
+  .controller('DashboardCtrl', ['$rootScope', '$scope', '$uibModal', 'NgTableParams', '$http', '$filter', function ($rootScope, $scope, $uibModal, NgTableParams, $http, $filter) {
+    $rootScope.init("dashboard");
     $scope.openModal = () => {
       $uibModal.open({
         animation: true,
@@ -14,23 +15,23 @@ angular.module('dataNewBorn')
         size: 'md',
         scope: $scope,
         controller: ['$scope', '$uibModalInstance', function ($scope, $uibModalInstance) {
-          $scope.content = 'This is modal content'
+          $scope.content = 'This is modal content';
           $scope.ok = function () {
             $uibModalInstance.close()
-          }
+          };
 
           $scope.cancel = function () {
             $uibModalInstance.dismiss('cancel')
           }
         }]
       })
-    }
-    $scope.selectedItem = null
+    };
+    $scope.selectedItem = null;
     $http.get('/api/dashboards/all').success((data) => {
-      $scope.themes = data
-    })
+      $scope.themes = data;
+    });
     $scope.selected = (item) => {
-      $scope.selectedItem = item
+      $scope.selectedItem = item;
       $.each(item.chartConfigDTOList, function (index, chart) {
         if (chart.chartType === 'PIE') {
           let pageload = {
@@ -47,24 +48,52 @@ angular.module('dataNewBorn')
               { x: 2009, y: 1012 },
               { x: 2010, y: 1012 }
             ]
-          }
+          };
           chart.config = {
             title: 'chart.label',
             debug: true
-          }
+          };
           chart.data = [ pageload ]
+        }else if(chart.chartType === 'TABLE'){
+          let data = [{name: "Moroni", age: 50},
+            {name: "Tiancum", age: 43},
+            {name: "Jacob", age: 27},
+            {name: "Nephi", age: 29},
+            {name: "Enos", age: 34},
+            {name: "Tiancum", age: 43},
+            {name: "Jacob", age: 27},
+            {name: "Nephi", age: 29},
+            {name: "Enos", age: 34},
+            {name: "Tiancum", age: 43},
+            {name: "Jacob", age: 27},
+            {name: "Nephi", age: 29},
+            {name: "Enos", age: 34},
+            {name: "Tiancum", age: 43},
+            {name: "Jacob", age: 27},
+            {name: "Nephi", age: 29},
+            {name: "Enos", age: 34}];
+          chart.columns = [
+            { title: 'Name', field: 'name', visible: true, filter: { 'name': 'text' } },
+            { title: 'Age', field: 'age', visible: true }
+          ];
+          chart.tableParams = new NgTableParams({
+            page: 1,            // show first page
+            count: 10,          // count per page
+            filter: {
+              name: 'M'       // initial filter
+            }
+          }, {
+            total: data.length, // length of data
+            getData: function(params) {
+              // use build-in angular filter
+              let orderedData = params.sorting() ?
+                $filter('orderBy')(data, params.orderBy()) :
+                data;
+
+              return orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+            }
+          });
         }
       })
-    }
-  // $scope.questionTable = new NgTableParams({
-  //   page: 1,
-  //   count: 10
-  // }, {
-  //   getData: function(params) {
-  //     return $http.get("/api/config/links", {}).then(function(response){
-  //       params.total(10)
-  //       return response.data
-  //     })
-  //   }
-  // })
-  }])
+    };
+  }]);
