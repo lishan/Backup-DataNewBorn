3422d0eb1218734d3c30413f4bfac71031e17a07
xingli <lixing.lx@sina.com>
2017-05-15 14:57:35 +0800

    backend api update

--- app/scripts/controllers/operation/rules.js
+++ app/scripts/controllers/operation/rules.js
@@ -3,7 +3,7 @@
 angular.module('dataNewBorn')
   .controller('OperationRulesCtrl', ['$scope', '$http', function ($scope, $http) {
     if ($scope.selectedItem) {
-      $http.get('/api/pivot-table-configs/' + $scope.selectedItem.id + '/statisticfields').success(function (data) {
+      $http.get('/api/pivottableconfigs/' + $scope.selectedItem.id + '/statisticfields').success(function (data) {
         $scope.selectedItem.statisticFields = data
       })
     }
