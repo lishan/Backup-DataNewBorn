27e116f8aa563ac77561c33e924f4e69cd1ced27
xingli <lixing.lx@sina.com>
2017-05-25 13:31:03 +0800

    rfm update

--- app/scripts/controllers/rfm.js
+++ app/scripts/controllers/rfm.js
@@ -4,51 +4,111 @@
  * Controller of the rfm
  */
 angular.module('dataNewBorn')
-  .controller('RfmCtrl', ['$rootScope', '$scope', '$http', 'Notification', 'NgTableParams', '$httpParamSerializerJQLike', '$window', function ($rootScope, $scope, $http, Notification, NgTableParams, $httpParamSerializerJQLike, $window) {
+  .controller('RfmCtrl', ['$rootScope', '$scope', '$http', 'Notification', 'NgTableParams', '$httpParamSerializerJQLike', '$window', '$filter', function ($rootScope, $scope, $http, Notification, NgTableParams, $httpParamSerializerJQLike, $window, $filter) {
     $rootScope.init('rfm')
     // TODO demo execution plan
     $scope.themes = [
       {
         id: 1,
-        label: '2016-11-01计划'
+        label: '2016-11-01方案'
       },
       {
         id: 2,
-        label: '2016-11-15计划'
+        label: '2016-11-15方案'
       },
       {
         id: 3,
-        label: '2016-12-01计划'
+        label: '2016-12-01方案'
       }
     ]
     $scope.selectedItem = $scope.themes[0]
 
     $scope.show = function () {
-      $http.get('/api/rfm/statistic/charts/').then(function (ret) {
-        $scope.charts = []
+      $http.post('/api/rfm/statistic/charts', {
+        startDate: $scope.startDate ? $filter('date')($scope.startDate, 'yyyy-MM-dd') : null,
+        endDate: $scope.endDate ? $filter('date')($scope.endDate, 'yyyy-MM-dd') : null,
+        startAmount: $scope.startAmount,
+        endAmount: $scope.endAmount,
+        orderStatus: $scope.selectedStatus ? $scope.selectedStatus.name : null,
+        gender: $scope.selectedGender ? $scope.selectedGender.name : null,
+        diliver: $scope.selectedDiliver ? $scope.selectedDiliver.name : null,
+        supplierId: $scope.selectedSupplierId ? $scope.selectedSupplierId.name : null
+      }).then(function (ret) {
+        // setup tables
+        $scope.queryDataTableData = ret.data
+        $scope.queryDataTableParams = new NgTableParams({}, { dataset: ret.data})
+        // setup charts
         let labels = ['金额', '订单量', '订单平均金额']
-        $.each(ret.data, function (index, chartData) {
+        $scope.charts = [{}, {}, {}]
+        for (let i = 0; i < 3; i++) {
           let chart = {}
           chart.config = {
-            title: labels[index],
-            showTitle: false
+            title: labels[i],
+            showTitle: false,
+            toolbox: {
+              show: true,
+              feature: {
+                dataView: {show: true, readOnly: true},
+                magicType: {show: true, type: ['line', 'bar']},
+                restore: {show: true},
+                saveAsImage: {show: true}
+              }
+            },
+            dataZoom: [
+              {
+                show: true,
+                start: 0,
+                end: 10
+              },
+              {
+                show: true,
+                yAxisIndex: 0,
+                filterMode: 'empty',
+                height: '80%',
+                showDataShadow: false,
+                left: '5%'
+              }
+            ]
           }
           let datapoints = []
-          $.each(chartData, function (index, dataRow) {
+          $.each(ret.data, function (index, dataRow) {
             let names = Object.getOwnPropertyNames(dataRow)
             datapoints.push({
               x: dataRow[names[0]],
-              y: dataRow[names[1]]
+              y: dataRow[names[i + 1]]
             })
           })
           chart.data = [{
             datapoints: datapoints
           }]
-          $scope.charts.push(chart)
-        })
+          $scope.charts[i] = chart
+        }
       })
     }
     $scope.show()
+
+    $scope.orderStatus = [
+      {name: '订单已完成', shade: 'dark'},
+      {name: '订单已取消', shade: 'light'},
+      {name: '正在制作', shade: 'dark'}
+    ]
+    $scope.selectedStatus = $scope.orderStatus[0]
+
+    $scope.genders = [
+      {name: '女士', shade: 'dark'},
+      {name: '先生', shade: 'light'}
+    ]
+
+    $scope.dilivers = [
+      {name: '自配送', shade: 'dark'},
+      {name: '百度物流', shade: 'light'}
+    ]
+
+    $scope.supplierIds = [
+      {name: '单体商户', shade: 'dark'},
+      {name: 'KA商户', shade: 'light'}
+    ]
+
     $scope.locations = [
       {name: 'GX高新一中商圈', shade: 'dark'},
       {name: 'GX高新路商圈', shade: 'light'},
@@ -56,6 +116,19 @@ angular.module('dataNewBorn')
       {name: 'CN西关商圈', shade: 'dark'},
       {name: 'BQ市政府商圈', shade: 'light'}
     ]
+
+    $scope.toggleSelection = function (item) {
+      item.isRowSelected = !item.isRowSelected
+      $scope.locations = []
+      $.each($scope.queryDataTableData, function (index, rowData) {
+        if (rowData.isRowSelected) {
+          $scope.locations.push({
+            name: rowData.bussinessLocation
+          })
+        }
+      })
+    }
+
     $scope.types = [
       {
         category: '重要价值客户',
@@ -71,9 +144,54 @@ angular.module('dataNewBorn')
         stars: [1, 2, 3, 4]
       }
     ]
+
     $scope.selectedLocation = $scope.locations[0] // red
 
     $scope.selectLocation = function () {
+      $scope.locationPie = {}
+      $scope.locationPie.config = {
+        title: $scope.selectedLocation.name,
+        showTitle: true,
+        grid: {
+          top: 0,
+          bottom: 0
+        }
+      }
+      $scope.locationPie.datapoints = []
+      $scope.locationPie.data = [{
+        datapoints: $scope.locationPie.datapoints
+      }]
+      $.each($scope.types, function (index, type) {
+        $scope.locationPie.datapoints.push({
+          x: type.category,
+          y: 1000
+        })
+      })
+      // setup tables
+      $scope.locationTable = {}
+      $scope.locationTable.data = []
+      $.each($scope.types, function (index, type) {
+        $scope.locationTable.data.push({
+          type: type.category,
+          count: 1000,
+          total: 100000,
+          mean: 100
+        })
+      })
+      $scope.locationTable.data.push({
+        type: '其他',
+        count: 1000,
+        total: 100000,
+        mean: 100
+      })
+      $scope.locationTable.data.push({
+        type: '总共',
+        count: 1000,
+        total: 100000,
+        mean: 100
+      })
+      $scope.locationTable.params = new NgTableParams({}, { dataset: $scope.locationTable.data})
+
       $scope.tables = []
       for (let i = 0; i < 3; i++) {
         let table = {
@@ -86,7 +204,14 @@ angular.module('dataNewBorn')
           {title: '用户名',
           field: 'username'},
           {title: '电话号码',
-          field: 'phone'}
+          field: 'phone'},
+          {title: '消费金额',
+            field: 'm',
+          valueFormatter: 'number:2'},
+          {title: '消费次数',
+          field: 'f'},
+          {title: '最近消费(N天前)',
+          field: 'r'}
         ]
         $http({
           method: 'POST',
@@ -121,11 +246,13 @@ angular.module('dataNewBorn')
         })
         $scope.tables.push(table)
       }
-    };
-    $scope.selectLocation();
+    }
+    $scope.selectLocation()
     // TODO
-    $scope.startdate = new Date(2016, 10, 1)
-    $scope.enddate = new Date(2016, 10, 14)
+    $scope.startDate = new Date(2016, 10, 1)
+    $scope.endDate = new Date(2016, 10, 14)
+    $scope.checkstartdate = new Date(2016, 10, 15)
+    $scope.checkenddate = new Date(2016, 10, 22)
 
     $scope.export = function (tableId) {
       let label = $scope.types[tableId].category + '.xls'
@@ -140,11 +267,11 @@ angular.module('dataNewBorn')
         Notification.success('导出成功！')
         $window.location.href = '/api/rfm/download/file?fileName=' + ret.data.file + '&label=' + label
       })
-    };
+    }
 
     $scope.buildRFMModel = function () {
       $http.post(
-        '/api/rfm/build'
+        '/api/rfm/build', {}
       ).then(function (response) {
         // Binding data
         $scope.result = response.data
@@ -152,5 +279,65 @@ angular.module('dataNewBorn')
       }, function (response) {
         $scope.result = response.data
       })
-    };
+    }
+
+    $scope.resultChartConfig = {
+      title: '投放用户消费对比',
+      subtitle: '',
+      yAxis: { scale: true },
+      debug: true,
+      stack: false
+    }
+    $scope.resultChartData = [
+      {
+        name: '上周',
+        datapoints: [
+          { x: 'GX高新一中商圈', y: 22 },
+          { x: 'GX高新路商圈', y: 13 },
+          { x: 'NQ小寨商圈', y: 35 },
+          { x: 'CN西关商圈', y: 52 },
+          { x: 'BQ市政府商圈', y: 32 }
+        ]
+      },
+      {
+        name: '本周',
+        datapoints: [
+          { x: 'GX高新一中商圈', y: 28 },
+          { x: 'GX高新路商圈', y: 19 },
+          { x: 'NQ小寨商圈', y: 55 },
+          { x: 'CN西关商圈', y: 82 },
+          { x: 'BQ市政府商圈', y: 38 }
+        ]
+      }
+    ]
+
+    $scope.resultChartConfig1 = {
+      title: '未投放用户消费对比',
+      subtitle: '',
+      yAxis: { scale: true },
+      debug: true,
+      stack: false
+    }
+    $scope.resultChartData1 = [
+      {
+        name: '上周',
+        datapoints: [
+          { x: 'GX高新一中商圈', y: 22 },
+          { x: 'GX高新路商圈', y: 13 },
+          { x: 'NQ小寨商圈', y: 35 },
+          { x: 'CN西关商圈', y: 52 },
+          { x: 'BQ市政府商圈', y: 32 }
+        ]
+      },
+      {
+        name: '本周',
+        datapoints: [
+          { x: 'GX高新一中商圈', y: 22 },
+          { x: 'GX高新路商圈', y: 15 },
+          { x: 'NQ小寨商圈', y: 33 },
+          { x: 'CN西关商圈', y: 49 },
+          { x: 'BQ市政府商圈', y: 29 }
+        ]
+      }
+    ]
   }])
